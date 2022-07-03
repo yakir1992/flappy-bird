d7c640e8b621235152588e2a0505c25b42153f2a
YakyAK <yakir1992@gmail.com>
2022-07-03 11:09:53 +0300

    upload project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { store, updateFrame, birdjump, game, states, rungame } from './store/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <App store={store} updateFrame={updateFrame} game={game}/>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function onpress(evt) {
+
+    switch (game.currentstate) {
+    default:
+    case states.Splash:
+      rungame()
+      birdjump(store.bird)
+      break
+    case states.Game:
+      birdjump(store.bird)
+      break
+    case states.Score:
+      break
+  }
+
+}
+
+document.addEventListener('mousedown', onpress);
